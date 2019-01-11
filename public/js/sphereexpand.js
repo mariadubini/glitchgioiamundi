04bab06ea61e6638806bb6e7b92010e09d4a2df5
Glitch (aframe) <none>
2019-01-11 21:39:09 +0000

    🐺👝 Checkpoint ./index.html:434942/185 ./public/js/sphereexpand.js:434942/16

--- public/js/sphereexpand.js
+++ public/js/sphereexpand.js
@@ -9,8 +9,7 @@
 
     
       
-      
-    var homeworldels = document.querySelectorAll(".baseworld");
+      var homeworldelements = document.querySelectorAll(".baseworld");
           for (var i = 0; i < homeworldels.length; i++) {
             homeworldels[i].setAttribute("visible", false);}
       
