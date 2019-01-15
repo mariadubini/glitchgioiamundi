f7d90a1e1488cbbf07c6f13a7d5a1f7cd59830b1
Glitch (aframe) <none>
2019-01-15 21:53:25 +0000

    🔑🐪 Checkpoint ./public/js/backhome.js:434942/1020 ./index.html:434942/144 ./public/js/sphereexpand.js:434942/68

--- public/js/backhome.js
+++ public/js/backhome.js
@@ -1,21 +1,20 @@
- AFRAME.registerComponent('sphereexpand', {
+ AFRAME.registerComponent('backhome', {
   schema: {
-      spheresource: {type: 'string'}},
+      homespheresource: {type: 'string'}},
     
    init: function () {
      
-
       let homeworldelements = document.querySelectorAll(".homeworld");
       let sky = document.querySelector("#sky");
       let data = this.data;
 
-      let sphereloader = () => {
-      sky.setAttribute("src", data.spheresource);
+      let gobackhome = () => {
+      sky.setAttribute("src", data.homespheresource);
       homeworldelements.forEach((homeworldelement) => {
-      homeworldelement.setAttribute("visible", false)})
+      homeworldelement.setAttribute("visible", true)})
     }
 
-      this.el.addEventListener('click', sphereloader);
+      this.el.addEventListener('click', gobackhome);
         
    }});
   
